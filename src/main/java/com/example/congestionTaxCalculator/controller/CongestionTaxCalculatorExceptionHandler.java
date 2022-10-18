01d9cf0096dcbfa027529a649fd16071f7d56048
Deepika <deepika.bhambhani@gmail.com>
2022-10-18 12:41:09 +0200

    Refactored code

--- src/main/java/com/example/congestionTaxCalculator/controller/CongestionTaxCalculatorExceptionHandler.java
+++ src/main/java/com/example/congestionTaxCalculator/controller/CongestionTaxCalculatorExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.example.congestionTaxCalculator.config;
+package com.example.congestionTaxCalculator.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
